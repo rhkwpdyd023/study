4b83bb83c6ab220301f9c601a6f9b8af345d5814
rhkwpdyd023 <rhkwpdyd023@gmail.com>
2022-09-21 21:36:20 +0900

    BJ 1

---
+++ BAEKJOON/1000.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) throws java.io.IOException{
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a+b);
+    }
+}
