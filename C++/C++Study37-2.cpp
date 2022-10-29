8a269dc2e2587423b5656daf9797c72980f48773
rhkwpdyd023 <rhkwpdyd023@gmail.com>
2022-10-29 20:06:23 +0900

    C++ Study 37

---
+++ C++/C++Study37-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iterator>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    vector<int> vc = {1, 2, 3, 4, 5};
+	copy(vc.begin(), vc.end(), ostream_iterator<int>(cout));
+	cout << endl;
+	copy(vc.begin(), vc.end(), ostream_iterator<int>(cout, " "));
+	return 0;
+    
+}
