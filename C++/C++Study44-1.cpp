3ec681fbddb78594233c2a518388ef817c767f27
rhkwpdyd023 <rhkwpdyd023@gmail.com>
2022-11-05 23:39:31 +0900

    C++ Study 44

---
+++ C++/C++Study44-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <functional>
+using namespace std;
+
+int main(){
+
+    plus<int> add;
+	equal_to<int> comp;
+	greater_equal<int> ge;
+	
+	cout << add(7, 3) << endl;
+	cout << comp(7, 3) << endl;
+	cout << ge(7, 3);
+	return 0;
+    
+}
