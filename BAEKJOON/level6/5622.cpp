9cbdc9c81b43d83de2cb76769a7454ea0c817c57
rhkwpdyd023 <rhkwpdyd023@gmail.com>
2022-12-03 21:06:12 +0900

    baek level 6

---
+++ BAEKJOON/level6/5622.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    string word;
+    int sec;
+
+    for(int i=0;i<word.length();i++){
+        
+    }
+
+    cout << sec << endl;
+
+    return 0;
+    
+}
