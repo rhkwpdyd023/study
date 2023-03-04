e36d3c72e9dfd30de4967670dfbf506431a0d3c3
rhkwpdyd023 <rhkwpdyd023@gmail.com>
2023-03-04 21:25:48 +0900

    100 WD day 33

--- 100DWebDevelopment/project7/operations.js
+++ 100DWebDevelopment/project7/operations.js
@@ -10,3 +10,13 @@ console.log(10 % 3);
 console.log((10+3-5)*10);
 
 let result = (10 + 3 -5) * 10;
+
+console.log('max' + ' ' + 'Schwarzmuller');
+console.log('max' - 'ax');
+
+let userName = 'Max';
+console.log(userName.length);
+console.log(userName.toUpperCase());
+
+let hobbies = ['Sports', 'Cooking'];
+console.log(hobbies.length);
