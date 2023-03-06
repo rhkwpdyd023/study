85a729f55025921cb20eb7b7f7ee12df8a07be31
rhkwpdyd023 <rhkwpdyd023@gmail.com>
2023-03-06 23:38:45 +0900

    100 WD day 34

--- 100DWebDevelopment/project8/app.js
+++ 100DWebDevelopment/project8/app.js
@@ -1,1 +1,15 @@
-document.body.children[1].children[0].href = 'https://google.com';
+// document.body.children[1].children[0].href = "https://google.com";
+
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = "https://google.com";
+
+anchorElement = document.querySelectorAll("p a");
+anchorElement.href = "https://academind.com";
+
+let newAnchorElement = document.createElement("a");
+newAnchorElement.href = "https://google.com";
+newAnchorElement.textContent = "this leads to Google!";
+
+let firstParagraph = document.querySelectorAll("p");
+
+firstParagraph.append(newAnchorElement);
