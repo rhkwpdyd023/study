3019cd36b326bd97e63035705dfab7ced84d07b8
rhkwpdyd023 <rhkwpdyd023@gmail.com>
2023-03-14 22:48:23 +0900

    100 WD day 38

--- 100DWebDevelopment/project8/conditional-code.js
+++ 100DWebDevelopment/project8/conditional-code.js
@@ -3,3 +3,15 @@ const myName = "Min";
 if (myName === "Min") {
   console.log("Hello!");
 }
+
+let isLoggedIn = true;
+
+if (!isLoggedIn) {
+  console.log("User is NOT logged in!");
+}
+
+const enteredUserName = 'Maximilian';
+
+if(enteredUserName.length>0){
+  console.log('Input is valid!');
+}
