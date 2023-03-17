8239adc599ad4dc358ec32df7cecd6ee684be725
rhkwpdyd023 <rhkwpdyd023@gmail.com>
2023-03-17 23:17:53 +0900

    100 WD day 39

--- 100DWebDevelopment/project8/loops.js
+++ 100DWebDevelopment/project8/loops.js
@@ -1,3 +1,29 @@
-for(let i =0;){
-
+for (let i = 0; i < 10; i++) {
+  console.log(i);
 }
+
+const users = ["Max", "Anna", "Joel"];
+
+for(const user of users){
+    console.log(user);
+}
+
+const loggedInUser = {
+    name: "Min",
+    age: 18,
+    isAdmin: true
+}
+
+for(const propertyName in loggedInUser){
+    console.log(propertyName);
+    console.log(loggedInUser[propertyName]);
+}
+
+let isFinished = false;
+
+while(!isFinished){
+    isFinished = confirm('do you want to quit?')
+}
+
+console.log('Done!');
+
