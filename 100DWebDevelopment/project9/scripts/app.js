61f7097518465c06552732e5015e3df4bca5329c
rhkwpdyd023 <rhkwpdyd023@gmail.com>
2023-03-21 23:07:42 +0900

    100 WD day 43

--- 100DWebDevelopment/project9/scripts/app.js
+++ 100DWebDevelopment/project9/scripts/app.js
@@ -1,16 +1,46 @@
+const gameData =[
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+]
+
+let editedPlayer = 0;
+let activePlayer = 0;
+
+const players = [
+  {
+    name: "",
+    symbol: "O",
+  },
+  {
+    name: "",
+    symbol: "X",
+  },
+];
+
 const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
-const formElement = document.querySelector('form');
-const errorsOutputElement = document.getElementById('config-errors')
+const formElement = document.querySelector("form");
+const errorsOutputElement = document.getElementById("config-errors");
+const gameAreaElement = document.getElementById("active-game");
+const activePlayerNameElement = document.getElementById('active-player-name');
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
-const cancelConfigBtnElement = document.getElementById("cancel-config-btb")
+const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
+const startNewGameBtnElement = document.getElementById("start-game-btn");
+const gameFieledElements = document.querySelectorAll("#game-board li");
 
-editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
-editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
+editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
+editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
 
-cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
-backdropElement.addEventListener('click', closePlayerConfig);
+cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
+backdropElement.addEventListener("click", closePlayerConfig);
 
-formElement.addEventListener('submit', savePlayerConfig);
+formElement.addEventListener("submit", savePlayerConfig);
+
+startNewGameBtnElement.addEventListener("click", startNewGame);
+
+for(const gameFieledElement of gameFieledElements){
+    gameFieledElement.addEventListener('click', selectGameField);
+}
--- 100DWebDevelopment/project9/scripts/config.js
+++ 100DWebDevelopment/project9/scripts/config.js
@@ -1,4 +1,6 @@
 function openPlayerConfig() {
+  editedPlayer = +event.target.dataset.playerid;
+
   playerConfigOverlayElement.style.display = "block";
   backdropElement.style.display = "block";
 }
@@ -8,6 +10,7 @@ function closePlayerConfig() {
   backdropElement.style.display = "none";
   formElement.firstElementChild.classList.remove("error");
   errorsOutputElement.textContent = "";
+  formElement.firstElementChild.lastElementChild.value = "";
 }
 
 function savePlayerConfig(event) {
@@ -21,5 +24,12 @@ function savePlayerConfig(event) {
     return;
   }
 
-  
+  const updatedPlayerDataElement = document.getElementById(
+    "player-" + editedPlayer + "-data"
+  );
+  updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+
+  players[editedPlayer - 1].name = enteredPlayername;
+
+  closePlayerConfig();
 }
