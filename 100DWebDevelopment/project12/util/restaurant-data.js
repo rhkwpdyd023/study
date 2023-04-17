da99b87acfc53204af79affdc02d263373ff2709
rhkwpdyd023 <rhkwpdyd023@gmail.com>
2023-04-17 23:58:49 +0900

    100 WD day 52

--- 100DWebDevelopment/project12/util/restaurant-data.js
+++ 100DWebDevelopment/project12/util/restaurant-data.js
@@ -3,4 +3,10 @@ function getStoredRestaurants() {
 
   const fileData = fs.readFileSync(filePath);
   const storedRestaurants = JSON.parse(fileData);
+
+  return storedRestaurants;
+}
+
+function storedRestaurants(storableRestaurants){
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
 }
